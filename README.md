cabd41ff9ac8d43efc93fc91928ef79e534db4ea
Yurii Zhurakovskyi <yuri.zhurakovsky@gmail.com>
2024-07-10 08:25:52 +0300

    Final

--- README.md
+++ README.md
@@ -3,8 +3,14 @@ Project's dependencies that should be installed on your Ubuntu 22.04.4 LTS:
 - [Kaggle API](https://www.endtoend.ai/tutorial/how-to-download-kaggle-datasets-on-ubuntu/)
 - NumPy
 
-Met errors:
+Used third-party libs:
+- https://vincela.com/csv/index.html
+- https://github.com/nlohmann/json
+
+Met errors (fixed):
 - https://github.com/vincentlaucsb/csv-parser/issues/115
 
 Split big csv file into smaller chunks:
 - https://www.datablist.com/learn/csv/split-big-csv-file-linux-mac-terminal
+
+Resulting JSON data stored to "build/result_output.json"
--- main.cpp
+++ main.cpp
@@ -1,130 +1,117 @@
+#include <algorithm>
+#include <cmath>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
-#include <unordered_map>
+#include <map>
+#include <memory>
 
-// #include <curl/curl.h>
 #include "csv.hpp"
-using csv::operator ""_csv;
+#include "nlohmann/json.hpp"
 
+using json = nlohmann::json;
 using namespace csv;
-
-
 using namespace std;
 namespace fs = std::filesystem;
 
-bool file_exists(const fs::path& p, fs::file_status s = fs::file_status{})
+template<typename A, typename B>
+std::pair<B,A> flip_pair(const std::pair<A,B> &p)
 {
-    std::cout << p;
-    if (fs::status_known(s) ? fs::exists(s) : fs::exists(p))
+    return std::pair<B,A>(p.second, p.first);
+}
+
+template<typename A, typename B>
+std::multimap<B,A> flip_map(const std::map<A,B> &src)
+{
+    std::multimap<B,A> dst;
+    std::transform(src.begin(), src.end(), std::inserter(dst, dst.begin()), 
+                   flip_pair<A,B>);
+    return dst;
+}
+
+void fill_container(const CSVRow& row, std::map<std::string, size_t>& container, const string& key)
+{
+    if (!row[key].is_null())
     {
-        std::cout << " exists\n";
-        return true;
-    }
-    else
-    {
-        std::cout << " does not exist\n";
-        return false;
+        if(row[key].is_str())
+        {
+            std::string key(row[key].get_sv());    
+            if(container.contains(key))
+            {
+                container[key] += 1;
+            }
+            else
+            {
+                container[key] = 1;
+            }
+        }
     }
 }
 
-
+template<typename A>
+void print_results_console(const A& container, const std::string& key)
+{
+    cout << key << ": " << endl;
+    for (const auto& item : container)
+    {
+        cout << "\t" << item.second << " : " << item.first << endl;
+    }
+}
 
 int main()
 {
-    // std::map<std::string_view, std::map<std::string_view, int>> big_data{};
-    std::unordered_map<std::string_view, int> big_data_category{};
-    std::unordered_map<std::string_view, int> big_data_maker{};
-    std::unordered_map<std::string_view, int> big_data_fuel{};
-
+    /***
+     * To be more explicit let's store all distributions in a separated containers
+     */
+    std::map<std::string, size_t> big_data_category{};
+    std::map<std::string, size_t> big_data_maker{};
+    std::map<std::string, size_t> big_data_fuel{};
 
     const fs::path datadirectory{"data"};
 
-    std::cout << "Current path is " << fs::current_path() << '\n';
-
-    if (!file_exists(datadirectory))
-    {
-        fs::create_directory(datadirectory);
-    }
-    else
-    {
-        cout << "Directory data exists!!!" << endl;
-    }
-
     CSVFormat format;
-    format.delimiter({ '\t',',','\n'})
-    .quote(false);
-    format.variable_columns(false); // Short-hand
+    format.delimiter(',')
+          .quote(false);
+    format.variable_columns(false);
     format.variable_columns(VariableColumnPolicy::IGNORE_ROW);
 
     for (auto const& dir_entry : fs::directory_iterator{datadirectory}) 
     {
-        //std::cout << dir_entry.path() << '\n';
-        std::string datapath = fs::path(dir_entry.path()).string();
-
-        auto info = get_file_info(datapath.c_str());
-        cout << "Number of rows in " << datapath << " : " << info.n_rows << endl;
-
-        CSVReader reader(datapath.c_str(), format);
-        // std::vector<std::string> col_names{reader.get_col_names()};
-        // for (const auto& col : col_names)
-        // {
-        //     cout << col << " ";
-        // }
-        // cout << endl;
-
-        for (const CSVRow& row : reader)
+        if (!dir_entry.is_regular_file())
         {
-            if (row.size() != 15)
-            {
-                continue;
-            }
-            
-            if (row["category"].is_str())
-            {
-                std::string_view value = row["category"].get_sv();
-                if (big_data_category.contains(value))
-                {
-                    big_data_category.at(value) += 1;
-                }
-                else
-                {
-                    big_data_category.try_emplace(value, 1);
-                }
-                
-            }
-            // big_data_category[row["category"].get_sv()] += 1;
-            // big_data_maker[row["makerName"].get_sv()] += 1;
-            // big_data_fuel[row["fuel"].get_sv()] += 1;
-            
-            // std::string_view makerName_field{row["category"].get_sv()};
-            // std::string_view makerName_field{row["makerName"].get_sv()};
-            // std::string_view fuel_field{row["fuel"].get_sv()};
-
+            continue;
         }
 
+        std::string datapath = fs::path(dir_entry.path()).string();
+        auto reader = std::unique_ptr<CSVReader>(new CSVReader(datapath.c_str(), format)); 
+
+        for (const CSVRow& row : *reader)
+        {
+            fill_container(row, big_data_fuel, "fuel");
+            fill_container(row, big_data_category, "category");
+            fill_container(row, big_data_maker, "makerName");
+        }
     }
 
-    cout << "category: " << endl;
-    for (const auto& item : big_data_category)
-    {
-        cout << "\t" << item.first << " : " << item.second << endl;
-    }
+    std::multimap<size_t, std::string> sorted_big_data_fuel = flip_map(big_data_fuel);
+    std::multimap<size_t, std::string> sorted_big_data_category = flip_map(big_data_category);
+    std::multimap<size_t, std::string> sorted_big_data_maker = flip_map(big_data_maker);
 
-    cout << "fuel: " << endl;
-    for (const auto& item : big_data_fuel)
-    {
-        cout << "\t" << item.first << " : " << item.second << endl;
-    }
+    print_results_console(sorted_big_data_fuel, "fuel");
+    print_results_console(sorted_big_data_category, "category");
+    print_results_console(sorted_big_data_maker, "makerName");
 
-    cout << "makerName: " << endl;
-    for (const auto& item : big_data_maker)
-    {
-        cout << "\t" << item.first << " : " << item.second << endl;
-    }
+    json j;
+    j["fuel"] = json(big_data_fuel);
+    j["category"] = json(big_data_category);
+    j["makerName"] = json(big_data_maker);
 
-    //cout << "Hello World!: " << datapath << endl;
+    // write prettified JSON to file
+    std::ofstream writeToFile("result_output.json");
+    writeToFile << std::setw(4) << j << std::endl;
+    writeToFile.close();
+
     return 0;
 }
